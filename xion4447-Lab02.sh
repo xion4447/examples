2a3deadfd39c8b0e83071f3ebf8d275926792433
Seng Ye Xiong <syxiong@stthomas.edu>
2019-02-16 02:56:12 +0000

    2nd commit

--- xion4447-Lab02.sh
+++ xion4447-Lab02.sh
@@ -4,8 +4,8 @@
 # Put commands below
 # To run, type ./YourID-Lab02.sh (you need the "./")
 # It must have permissions starting with a "7"
-mkdir -p main/{A/{B,C,D},C/{E,F},D/{G,H/{J,K},I}}
-touch main/{A/{aa,B/bb,C/cc,D/dd},C/{cc,E/ee,F/ff},D/{dd,G/gg,H/{hh,J/jj,K/kk},I/ii}}
+mkdir -p main/{A/{B,C,D},L/{E,F},M/{G,H/{J,K},I}}
+touch main/{A/{aa,B/bb,C/cc,D/dd},L/{ll,E/ee,F/ff},M/{mm,G/gg,H/{hh,J/jj,K/kk},I/ii}}
 sudo apt-get -y update
 sudo apt-get -y install tree
 sudo apt-get -y install git
